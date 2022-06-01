48cadf57281a287e7ad06065fb86a1a3a507c8fb
David Abdelmalek <davidonsy123@gmail.com>
2022-06-01 18:52:54 +0200

    upadte sol p394

--- leetcode/p394.py
+++ leetcode/p394.py
@@ -1,18 +1,43 @@
-class Solution(object):
-    def decodeString(self, s):
-        stack = []; curNum = 0; curString = ''
-        for c in s:
-            if c == '[':
-                stack.append(curString)
-                stack.append(curNum)
-                curString = ''
-                curNum = 0
-            elif c == ']':
-                num = stack.pop()
-                prevString = stack.pop()
-                curString = prevString + num*curString
-            elif c.isdigit():
-                curNum = curNum*10 + int(c)
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        output=''
+        # process input
+        prev = s[0]
+        for char in s[1:len(s)+1]:
+            if char.isnumeric() and prev.isnumeric():
+                prev += char
+            elif char.isalpha() and prev.isalpha():
+                prev += char
             else:
-                curString += c
-        return curString
+                stack.append(prev)
+                prev = char
+        stack.append(prev)
+
+        new_stack =[]
+        for char in stack:
+            if char == ']':
+                new_stack= recurs(new_stack[:-1],new_stack[-1])
+            else:
+                new_stack.append(char)
+        return ''.join(new_stack)
+            
+
+        
+    
+def recurs(stack,input_str) -> List:
+    if len(stack) == 0:
+        return []
+    char = stack[-1]
+    if char =='[':
+        stack.pop()
+        return recurs(stack,input_str)
+    elif char.isalpha():
+        # concatenate
+        stack.pop()
+        input_str = char+input_str
+        return recurs(stack,input_str)
+    elif char.isdigit():
+        stack.pop()
+        stack.append(int(char)* input_str)
+        return stack
